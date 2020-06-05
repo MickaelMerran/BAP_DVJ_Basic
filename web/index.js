ac49b9c045ec5e7ddfe19f8209b1531f90b5d1c3
Florian <flo78480@hotmail.fr>
2020-06-05 17:49:43 +0200

    Co-authored-by: mohamed-amin.chara@epitech.eu <mohamed-amin.chara@epitech.eu>

--- web/index.js
+++ web/index.js
@@ -1,21 +1,74 @@
-const slider = $("#project-slider");
+// const slider = $("#project-slider");
 
 
-slider
-.slick({
-arrows:false,
-centerMode: true,
-centerPadding: '100px',
-slidesToShow: 1,
-dots:false,
-infinite: true,
-variableWidth: false,
-vertical: true,
-cssEase: 'ease-in-out'
+// slider
+// .slick({
+// arrows:false,
+// centerMode: true,
+// centerPadding: '100px',
+// slidesToShow: 1,
+// dots:false,
+// infinite: true,
+// variableWidth: false,
+// vertical: true,
+// cssEase: 'ease-in-out'
 
-});
+// });
+getProject1();
+var $slider = $("#project-slider");
+var index;
+const url = "http://localhost:1337";
+let allProject = [];
 
-slider.on('DOMMouseScroll mousewheel whee', (function(e) {
+if ($slider.length) {
+  var currentSlide;
+  var slidesCount;
+  
+  var updateSliderCounter = function(slick, currentIndex) {
+    currentSlide = slick.slickCurrentSlide() + 1;
+    slidesCount = slick.slideCount;
+    index = (currentSlide + '/' +slidesCount);
+    console.log(index);
+  };
+
+  $slider.on('init', function(event, slick) {
+    updateSliderCounter(slick);
+  });
+
+  $slider.on('afterChange', function(event, slick, currentSlide) {
+    updateSliderCounter(slick, currentSlide);
+  });
+
+  $slider.slick({
+    arrows:false,
+    centerMode: true,
+    centerPadding: '100px',
+    slidesToShow: 1,
+    dots:false,
+    infinite: true,
+    variableWidth: false,
+    vertical: true,
+    cssEase: 'ease-in-out'
+    
+  });
+}
+
+function getProject1() {
+  var div = document.createElement('div');
+  if  (index >= 1) {
+    div.innerHTML = '';
+    div.innerHTML +=
+        '<span style="display:inline;"><img class="imgone" src="'+url+allProject+PhotoOne[0].url+'"></span>';
+    document.getElementById('imageOne').appendChild(div);
+  }   if  (index >= 2) {
+    div.innerHTML = '';
+    div.innerHTML +=
+        '<span style="display:inline;"><img class="imgone" src="'+"https://www.w3schools.com/css/img_forest.jpg"+'"></span>';
+    document.getElementById('imageOne').appendChild(div);
+  }
+}
+
+$slider.on('DOMMouseScroll mousewheel whee', (function(e) {
   e.preventDefault();
 
   if (e.originalEvent.deltaY < 0) {
@@ -24,5 +77,3 @@ slider.on('DOMMouseScroll mousewheel whee', (function(e) {
     $(this).slick('slickNext');
   }
 }));
-
-const swup = new Swup();
